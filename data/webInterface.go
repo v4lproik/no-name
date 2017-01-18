47a48d10698199c8921ec61882ff83e34eec7ad0
jrousseau <jrousseau@octo.com>
2017-01-18 20:53:29 +1100

    [data] Move form struct to domain external package

--- data/webInterface.go
+++ data/webInterface.go
@@ -3,15 +3,16 @@ package data
 import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/yinkozi/no-name/client"
+	"github.com/yinkozi/no-name-domain"
 )
 
 type WebInterface struct {
 	ClientWeb *client.Web
 
 	Doc *goquery.Document
-	Form *Form
+	Form *domain.Form
 }
 
 func NewWebInterface(webClient *client.Web) *WebInterface{
-	return &WebInterface{webClient, nil, NewForm()}
+	return &WebInterface{webClient, nil, domain.NewForm()}
 }
