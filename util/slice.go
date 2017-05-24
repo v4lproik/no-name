32bc365655c7f33d88b716230823b072c30684a4
jrousseau <jrousseau@octo.com>
2017-05-24 14:36:38 +0200

    Add regex for specific form inputs detection

--- util/slice.go
+++ util/slice.go
@@ -5,9 +5,13 @@ import (
 )
 
 func Contains(stringSlice []string, searchString string) bool {
-	for _, value := range stringSlice {
-		if value == searchString {
+	for _, regex := range stringSlice {
+		match, err := regexp.MatchString(regex, searchString)
+		if match {
 			return true
+		}
+		if err != nil {
+			loggerUtilHTML.Errorf(err.Error())
 		}
 	}
 	return false
